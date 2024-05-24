d473458e9a29b62035214d2710e51d09f261c4f8
Dorany Braga <doranygoncalvesbraga@gmail.com>
2024-05-24 15:36:46 -0300

    Create estruturaDeDados_uni1_at1.c
    Ponteiros_ atividade 1

---
+++ estruturaDeDados_uni1_at1.c
@@ -0,0 +1,24 @@
+/*
+ATIVIDADE 1:Faça um pequeno programa para testar seus conhecimentos sobre ponteiros e 
+alocação na memória.
+
+A)defina um ponteiro do tipo inteiro
+B) Faça alocação dinâmica para o ponteiro recém-criado
+C) Preencha a memória com o valor 42
+D) Imprima o endereço do poneiro na memória e o valor contido nele*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int *ponteiro; // define um ponteiro de inteiro
+	ponteiro = (int *)malloc(sizeof(int)); //calocação dinâmica do ponteiro
+	
+	*ponteiro = 42; //preencha a memória com o valor 42
+
+	//imprima o endereço do ponteiro na memória e o valor contido nele
+	printf ("Endereço: %d\nValor: %d\n\n, ponteiro, *ponteiro");
+
+	system("pause");
+	return(0);
+}
