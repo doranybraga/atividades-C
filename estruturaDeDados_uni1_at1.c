f8087251b8c6e4d039838f9ea73b9a6692357b79
Dorany Braga <doranygoncalvesbraga@gmail.com>
2024-05-27 19:54:16 -0300

    estruturaDeDados_uni1_at1.
    estruturaDeDados_uni1_at1.

--- estruturaDeDados_uni1_at1.c
+++ estruturaDeDados_uni1_at1.c
@@ -2,7 +2,7 @@
 ATIVIDADE 1:Faça um pequeno programa para testar seus conhecimentos sobre ponteiros e 
 alocação na memória.
 
-A)defina um ponteiro do tipo inteiro
+A) Defina um ponteiro do tipo inteiro
 B) Faça alocação dinâmica para o ponteiro recém-criado
 C) Preencha a memória com o valor 42
 D) Imprima o endereço do poneiro na memória e o valor contido nele*/
@@ -17,7 +17,7 @@ int main(){
 	*ponteiro = 42; //preencha a memória com o valor 42
 
 	//imprima o endereço do ponteiro na memória e o valor contido nele
-	printf ("Endereço: %d\nValor: %d\n\n, ponteiro, *ponteiro");
+	printf ("Endereço: %d\nValor: %d\n\n", ponteiro, *ponteiro);
 
 	system("pause");
 	return(0);
